f3bde916d9940c986e544a861c00178ba9de148e
Shreeyanshi Gautam <67256100+shreeyanshi-2111@users.noreply.github.com>
2024-05-10 12:13:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+from collections import OrderedDict
+
+app = Flask(_name_)
+
+# Sample to-do list (replace with database interaction for persistence)
+todos = OrderedDict()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    # Add new todo
+    todo = request.form['todo']
+    if todo:
+      todos[todo] = False  # Mark new todo as incomplete
+      return redirect(url_for('index'))  # Redirect to avoid form resubmission
+  return render_template('index.html', todos=todos)
+
+@app.route('/completed/<todo>', methods=['GET'])
+def complete(todo):
+  if todo in todos:
+    todos[todo] = True  # Mark todo as completed
+  return redirect(url_for('index'))
+
+@app.route('/delete/<todo>', methods=['GET'])
+def delete(todo):
+  if todo in todos:
+    del todos[todo]  # Delete todo
+  return redirect(url_for('index'))
+
+if _name_ == '_main_':
+  app.run(debug=True)
